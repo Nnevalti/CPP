2f1b38b8b6da700701e3fc60a90706e4d7dc70f2
Nnevalti <deschamps.valentin.1998@gmail.com>
2021-05-31 16:45:17 +0200

    Ouais

--- cpp04/ex03/main.cpp
+++ cpp04/ex03/main.cpp
@@ -38,32 +38,32 @@ int main()
     src->learnMateria(new Ice());
     src->learnMateria(new Cure());
 
-    ICharacter* moi = new Character("moi");
+    ICharacter* me = new Character("me");
 
     AMateria* tmp = NULL;
     AMateria* tmp2 = NULL;
     tmp = src->createMateria("ice");
-    moi->equip(tmp);
+    me->equip(tmp);
     std::cout << "ICE = " << tmp->getType() << std::endl;
 
     tmp2 = src->createMateria("cure");
-    moi->equip(tmp2);
+    me->equip(tmp2);
     std::cout << "CURE = " << tmp2->getType() << std::endl;
 
     ICharacter* bob = new Character("bob");
 
-    moi->use(0, *bob);
-    moi->use(1, *bob);
-    std::cout << tmp->getType() << " xp = " << tmp->getXP() << std::endl;
-    std::cout << tmp2->getType() << " xp = " << tmp2->getXP() << std::endl;
+    me->use(0, *bob);
+    me->use(1, *bob);
+    std::cout << tmp->getType() << " xp: " << tmp->getXP() << std::endl;
+    std::cout << tmp2->getType() << " xp: " << tmp2->getXP() << std::endl;
 
 
-    moi->use(0, *bob);
-    std::cout << tmp->getType() << " xp = " << tmp->getXP() << std::endl;
-    std::cout << tmp2->getType() << " xp = " << tmp2->getXP() << std::endl;
+    me->use(0, *bob);
+    std::cout << tmp->getType() << " xp: " << tmp->getXP() << std::endl;
+    std::cout << tmp2->getType() << " xp: " << tmp2->getXP() << std::endl;
 
     delete bob;
-    delete moi;
+    delete me;
     delete src;
 
     return 0;
